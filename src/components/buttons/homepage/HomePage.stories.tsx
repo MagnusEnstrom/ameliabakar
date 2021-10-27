38982f04e91b160a6be87b3063d8d2b8716443a2
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2021-10-27 23:07:39 +0200

    removed disabled from homePage button

--- src/components/buttons/homepage/HomePage.stories.tsx
+++ src/components/buttons/homepage/HomePage.stories.tsx
@@ -18,13 +18,5 @@ const Template = (args) => <Button disabled={args.disabled}>{args.label}</Button
 export const Initial = Template.bind({});
 
 Initial.args = {
-  disabled: false,
-  label: 'Button',
-};
-
-export const Disabled = Template.bind({});
-
-Disabled.args = {
-  disabled: true,
   label: 'Button',
 };
