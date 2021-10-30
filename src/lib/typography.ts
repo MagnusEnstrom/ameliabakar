ada06bc33df608bb36ee6e9c899b290f761e2e8e
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2021-10-30 10:06:43 +0200

    typography

--- src/lib/typography.ts
+++ src/lib/typography.ts
@@ -1,8 +1,119 @@
+const headerFontFamily = "'Lusitana', serif"; 
+const textFontFamily = "'Nunito', sans-serif"; 
+
+const h1Mobile = {
+    fontWheight: 700,
+    fontSize: '24px',
+    lineHeight: '150%',
+    fontFamily: headerFontFamily,
+}
+
+const h2Mobile = {
+    fontWheight: 700,
+    fontSize: '20px',
+    lineHeight: '150%',
+    fontFamily: headerFontFamily,
+}
+
+const cardMobile = {
+    fontWheight: 700,
+    fontSize: '14px',
+    lineHeight: '120%',
+    fontFamily: textFontFamily,
+}
+
+const badge  = {
+    fontWheight: 800,
+    fontSize: '10px',
+    lineHeight: '100%',
+    fontFamily: textFontFamily,
+}
+const note  = {
+    fontWheight: 600,
+    fontSize: '12px',
+    lineHeight: '100%',
+    fontFamily: textFontFamily,
+}
+const p  = {
+    fontWheight: 300,
+    fontSize: '16px',
+    lineHeight: '140%',
+    fontFamily: textFontFamily,
+}
+const nav  = {
+    fontWheight: 400,
+    fontSize: '18px',
+    lineHeight: '100%',
+    fontFamily: textFontFamily,
+}
+const button  = {
+    fontWheight: 700,
+    fontSize: '15px',
+    lineHeight: '100%',
+    fontFamily: textFontFamily,
+}
+const card  = {
+    fontWheight: 700,
+    fontSize: '16px',
+    lineHeight: '120%',
+    fontFamily: textFontFamily,
+}
+const h1 = {
+    fontWheight: 700,
+    fontSize: '40px',
+    lineHeight: '150%',
+    fontFamily: headerFontFamily,
+}
+const h2 = {
+    fontWheight: 700,
+    fontSize: '32px',
+    lineHeight: '130%',
+    fontFamily: headerFontFamily,
+}
+
+const h3 = {
+    fontWheight: 700,
+    fontSize: '24px',
+    lineHeight: '150%',
+    fontFamily: headerFontFamily,
+}
+
+const h4 = {
+    fontWheight: 700,
+    fontSize: '20px',
+    lineHeight: '150%',
+    fontFamily: headerFontFamily,
+}
+
+const h5 = {
+    fontWheight: 700,
+    fontSize: '16px',
+    lineHeight: '150%',
+    fontFamily: headerFontFamily,
+}
+const h6 = {
+    fontWheight: 700,
+    fontSize: '14px',
+    lineHeight: '150%',
+    fontFamily: headerFontFamily,
+}
+
 const typography = {
-    //
-    h1Mobile: {
-        
-    }
+    h1Mobile,
+    h2Mobile,
+    cardMobile,
+    badge,
+    note,
+    p,
+    nav,
+    button,
+    card,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6
 }
 
 export default typography;
