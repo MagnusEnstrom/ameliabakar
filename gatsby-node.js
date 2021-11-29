b181e99cf0ab7ac97e5f3a01f8c9da3aba769b5c
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2021-11-29 09:13:38 +0100

    homepage setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,18 +14,26 @@ const chunk = require(`lodash/chunk`)
 
 exports.createPages = async gatsbyUtilities => {
     const { actions, graphql, reporter} = gatsbyUtilities
+
+    await createRecipeDetailPage(gatsbyUtilities);
+    await createHomePage(gatsbyUtilities);
+  
+}
+
+const createRecipeDetailPage = async ({ actions, graphql, reporter}) => {
     const result = await graphql(`
     {
-        allWpRecept {
+        allWpRecept(sort: { fields: [date] order: DESC}) {
           nodes {
             id
             uri
             title
+                  date
             content
             tags {
-                nodes {
-                    name
-                }
+              nodes {
+                name
+              }
             }
             singlePaketAfc {
               tooltip
@@ -50,8 +58,7 @@ exports.createPages = async gatsbyUtilities => {
             }
           }
         }
-      }
-      
+      }      
   `)
   if (result.errors) {
     reporter.error("There was an error fetching posts", result.errors)
@@ -60,7 +67,7 @@ exports.createPages = async gatsbyUtilities => {
   const { allWpRecept } = result.data
   
   // Define the template to use
-  const template = require.resolve(`./src/templates/receptPost.tsx`)
+  const receptPost = require.resolve(`./src/templates/receptPost.tsx`)
   
   if (allWpRecept.nodes.length) {
     allWpRecept.nodes.map(recept => {
@@ -68,11 +75,67 @@ exports.createPages = async gatsbyUtilities => {
         // It's best practice to use the uri field from WPGraphQL nodes when
         // building
         path: recept.uri,
-        component: template,
+        component: receptPost,
         context: recept,
       })
     })
   }
+} 
+
+const createHomePage = async ({ actions, graphql, reporter}) => {
+    const result = await graphql(`
+    {
+        allWpRecept {
+            nodes {
+                id
+                uri
+                title
+                content
+            tags {
+                nodes {
+                    name
+                }
+            }
+            singlePaketAfc {
+                tooltip
+                tips
+                tidFormat
+                tid
+                svarighetsgrad
+                saHarGorDu
+                kortBeskrivning
+                images {
+                    localFile {
+                        childrenImageSharp {
+                            original {
+                                src
+                            }
+                            fixed(width: 400, height: 400) {
+                                src
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        }
+    }
+      
+    `)
+    if (result.errors) {
+        reporter.error("There was an error fetching posts", result.errors)
+    }
+  
+    const { allWpRecept } = result.data
+  
+    // Define the template to use
+    const home = require.resolve(`./src/templates/home.tsx`)
+  
+    actions.createPage({
+        path: '/',
+        component: home,
+        context: allWpRecept,
+    })
 }
 
 /**
---
+++ src/templates/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+    return (
+        <div>
+            home poggers
+        </div>
+    )
+}
+
+export default home
