5d34eeb9761aa4578760b0bd383816efc37ee6f3
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-02-27 19:30:21 +0100

    updated tests

--- cypress/integration/recipe.spec.ts
+++ cypress/integration/recipe.spec.ts
@@ -3,50 +3,50 @@
 describe('/recept', () => {
     beforeEach(() => {
         cy.visit('/recept')
-    });
+    })
 
     it('A user can see a list of 10 recipe cards & load more', () => {
         // shows recipes
-        cy.findAllByLabelText('recept').should('have.length', 10);
+        cy.findAllByLabelText('recept').should('have.length', 10)
         // loads in more recipes
-        cy.get('[data-cy=loadmore]').click();
-        cy.findAllByLabelText('recept').should('have.length', 14);
+        cy.get('[data-cy=loadmore]').click()
+        cy.findAllByLabelText('recept').should('have.length', 14)
     })
 
-    it('shows filter & hide filter', () => {
+    it.only('shows filter & hide filter', () => {
         cy.contains('Recept')
         //Filter list is not shown
-        cy.get('[data-cy=filterButton]').should('not.exist');
-        cy.findByLabelText('close filter').should('not.exist');
+        cy.get('[data-cy=filterButton]').should('not.be.visible')
+        cy.findByLabelText('close filter').should('not.exist')
         const filterButton = cy.findByLabelText('show filter')
-        filterButton.click();
-        
+        filterButton.click()
+
         //Filter list is
-        cy.get('[data-cy=filterButton]').should('exist');
-        const closeButton = cy.findByLabelText('close filter');
-        closeButton.click();
-        
+        cy.get('[data-cy=filterButton]').should('exist')
+        const closeButton = cy.findByLabelText('close filter')
+        closeButton.click()
+
         //Filter list is not shown
-        cy.findByLabelText('close filter').should('not.exist');
-        cy.get('[data-cy=filterButton]').should('not.exist');
-        
+        cy.findByLabelText('close filter').should('not.exist')
+        cy.get('[data-cy=filterButton]').should('not.be.visible')
     })
-    
+
     it('can save & view saved recipes', () => {
         cy.visit('/mina-recept')
-        cy.findByRole('heading', { name: /Dina sparade recept kommer att visas här/i})
+        cy.findByRole('heading', {
+            name: /Dina sparade recept kommer att visas här/i,
+        })
         cy.findByText(/Du har för närvarande inte några sparade recept/i)
-        cy.findByRole('button', { name: /Här hittar du mina recept/i}).click();
-        
-        cy.findAllByLabelText('recept').should('have.length', 10);
+        cy.findByRole('button', { name: /Här hittar du mina recept/i }).click()
+
+        cy.findAllByLabelText('recept').should('have.length', 10)
         cy.get('[data-cy=heartButton]')
-        .should('not.have.attr', 'aria-selected', 'true')
-        .click({multiple: true})
-        .should('have.attr', 'aria-selected', 'true');
-        
-        
+            .should('not.have.attr', 'aria-selected', 'true')
+            .click({ multiple: true })
+            .should('have.attr', 'aria-selected', 'true')
+
         cy.visit('/mina-recept')
-        cy.findByRole('heading', { name: /Mina sparade recept/i});
-        cy.findAllByLabelText('recept').should('have.length', 10);
+        cy.findByRole('heading', { name: /Mina sparade recept/i })
+        cy.findAllByLabelText('recept').should('have.length', 10)
     })
-  })
+})
